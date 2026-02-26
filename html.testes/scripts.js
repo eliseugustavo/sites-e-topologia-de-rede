237dbd5a1ec26110b8e690b487951655fdc464d5
eliseugustavo <eliseugustavo2@gmail.com>
2026-02-25 21:56:04 -0300

    atualização do site

---
+++ html.testes/scripts.js
@@ -0,0 +1,23 @@
+/* 
+[]botao
+[]quando o botao é clicado
+[]textarea
+[]pegar o valor do textarea
+[]enviar para a IA
+[]peagr reposta da IA e colocar na tela
+
+
+1°palavra chave:
+2°palavra chave: redbull
+3°palavra chave:
+4°palavra chave:
+
+*/
+
+
+
+
+
+
+
+
